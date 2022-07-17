517e405c01bdd6c01a9b58383a0587e35cf43a0f
daedaluslux <daedaluslux@gmail.com>
2022-07-17 20:57:15 +0200

    New Marketplace

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+let secrets = require("./secret.json");
+
+
+
+module.exports = {
+  defaultNetwork: "mainnet",
+  networks: {
+    hardhat: {
+    },
+    mainnet: {
+      url: "https://bsc-dataseed1.binance.org",
+      accounts: [secrets.Pkey]
+    },
+
+    Bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [secrets.Pkey]
+    },
+
+    RinkebyTestner: {
+      url: `https://rinkeby.infura.io/v3/${secrets.InfuraId}`,
+      accounts: [secrets.Pkey]
+    }
+  },
+
+
+
+
+
+  etherscan: {
+    apiKey: {
+      bscTestnet: "8FC3WJDD55GHZS6B33ZTZEIH9GZ4JJWMPK"
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+
+  mocha: {
+    timeout: 40000
+  }
+}
+
+
